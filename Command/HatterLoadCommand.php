9934db68f2f832315b7716fec4f8b262d31dde45
achianumba <chianumbaarinze@gmail.com>
2025-01-29 21:58:13 +0100

    feat: make hatter a composer package #0000

--- Command/HatterLoadCommand.php
+++ Command/HatterLoadCommand.php
@@ -1,27 +1,43 @@
 <?php
 
-namespace App\Command;
+namespace LinkORB\Component\Hatter\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use LinkORB\Component\Hatter\Factory\HatterFactory;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Yaml\Yaml;
 use Connector\Connector;
+use Symfony\Component\Dotenv\Dotenv;
 
 class HatterLoadCommand extends Command
 {
     protected static $defaultName = 'hatter:load';
+    private string $dsn;
 
     // get environment variable `dsn` during construction
-    public function __construct(ParameterBagInterface $params)
+    public function __construct()
     {
-        $this->dsn = $params->get('dsn');
+        $projectRoot = dirname(dirname(dirname(dirname(__DIR__))));
+        $envFile = $projectRoot . '/.env.local';
+
+        if (!file_exists($envFile)) {
+            fwrite(STDERR, PHP_EOL . 'Missing file: ' . $envFile . ' does not exist.' . PHP_EOL . PHP_EOL);
+            exit(1);
+        }
+
+        $dotenv = new Dotenv();
+        $dotenv->load($envFile);
+
+        if (!array_key_exists('HATTER_DSN', $_ENV)) {
+            fwrite(STDERR, PHP_EOL . 'Error: Missing environment variable: Make sure to set the "HATTER_DSN" environment variable to your database connection string in the .env.local file.' . PHP_EOL . PHP_EOL);
+            exit(1);
+        }
+
+        $this->dsn = $_ENV['HATTER_DSN'];
         parent::__construct();
     }
-
 
     protected function configure(): void
     {
@@ -33,10 +49,9 @@ class HatterLoadCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $filenames = $input->getArgument('filenames');
-        // print_r($filenames);exit();
 
         $hatter = HatterFactory::fromFilenames($filenames);
-        
+
         $connector = new Connector();
         $config = $connector->getConfig($this->dsn);
         if (!$connector->exists($config)) {
